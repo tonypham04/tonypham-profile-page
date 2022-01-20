52fb68780672e7543335d65cb7a0a39284c126bc
Tony Pham <tonyphamx04@gmail.com>
2022-01-20 17:46:50 -0500

    Interface: Add interface to represent a project and list of projects

---
+++ src/app/projects.ts
@@ -0,0 +1,31 @@
+export interface Project {
+    id: number;
+    name: string;
+    imagePath: string;
+    imageDescription: string;
+    description: string;
+}
+
+export const projects = [
+    {
+        id: 1,
+        name: 'Responsive Website',
+        imagePath: '',
+        imageDescription: '',
+        description: ''
+    },
+    {
+        id: 2,
+        name: 'Music and Hardware',
+        imagePath: '',
+        imageDescription: '',
+        description: ''
+    },
+    {
+        id: 3,
+        name: 'Retro Game',
+        imagePath: '',
+        imageDescription: '',
+        description: ''
+    }
+];
