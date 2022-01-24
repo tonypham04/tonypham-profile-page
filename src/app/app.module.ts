539830ae8121ad2b3578bae35a7253f807227f3a
Tony Pham <tonyphamx04@gmail.com>
2022-01-24 17:44:28 -0500

    Project details: Add scaffolded code to add project-details into the main app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 import { ProfileIntroComponent } from './profile-intro/profile-intro.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileHeaderComponent,
-    ProfileIntroComponent
+    ProfileIntroComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule
