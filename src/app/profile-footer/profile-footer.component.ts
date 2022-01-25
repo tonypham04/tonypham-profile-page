2196133dda2ae8fcd6e19705c162cbad5fefaa98
Tony Pham <tonyphamx04@gmail.com>
2022-01-25 17:11:48 -0500

    Footer: Add scaffolded footer component from running 'ng generate component profile-footer'

---
+++ src/app/profile-footer/profile-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-footer',
+  templateUrl: './profile-footer.component.html',
+  styleUrls: ['./profile-footer.component.css']
+})
+export class ProfileFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
